033bee27b4eda2d4aab022cd3c2eb90774b0b3a1
lakshmankotiki <kotiki.lakshman1996@gmail.com>
2020-09-23 07:39:29 +0530

    counter app basic files with counter component

---
+++ src/counter.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+class Counter extends React.Component {
+    state = {
+
+    };
+    render() {
+        return <h1>Hello Lakshman!</h1>
+    }
+}
+
+export default Counter;
