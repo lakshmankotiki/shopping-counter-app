1c0f8ba6f76c8d8e3be6ef62e5936703c86cc87b
lakshmankotiki <kotiki.lakshman1996@gmail.com>
2020-09-26 13:33:51 +0530

    updated the render() based on conditions

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -2,15 +2,38 @@ import React from 'react';
 
 class Counter extends React.Component {
     state = {
-        count: 0
+        count: 0,
+        tags: [
+            {
+                id: 12121,
+                name: "John",
+                Job: "Software Engineer",
+            },
+            {
+                id: 98481,
+                name: "Mike",
+                Job: "HR"
+            },
+            {
+                id: 74738,
+                name: "Kathryn",
+                Job: "Quality Analyst"
+            }
+        ]
     };
+
+    handleIncrement = () => {
+        console.log(this);
+    };
+
     render() {
         return(
             // Fragments lets you group a list of children without adding extra nodes as parent which is of useless
             <React.Fragment>
                 <h1><u>Shopping Counter App</u></h1>
                 <span className={ this.getBadgeClasses() }>{ this.formatCount() }</span>
-                <button className="btn btn-secondary btn-sm">Increment</button>
+                <button onClick={ this.handleIncrement } className="btn btn-secondary btn-sm">Increment</button>
+                {this.state.tags.length === 0 ? 'Please give the array Items' : <ul>{this.state.tags.map(tag => <li key={tag.id}>{ tag.Job }</li>)}</ul>}
             </React.Fragment>
         )
     }
